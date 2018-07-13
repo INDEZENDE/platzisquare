ffc130d3e7aa127d369e921e467316c56a3f2ac0
Indelisa Rios <Indelisa.Rios@gft.com>
2018-07-13 17:30:13 -0500

    ngFor practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Cambio en .ts';
+  lugares:any = [
+    {cercania: 1, distancia:10, active: true, nombre: 'Floreria'},
+    {cercania: 2, distancia:20, active: true, nombre: 'Restaurante'},
+    {cercania: 3, distancia:30, active: false, nombre: 'Veterinaria'},
+  ];
+
+  // a=3;
+  // b=5;
+  // listo=false;
+
+  constructor(){
+  //   setTimeout(()=>{this.listo=true},3000)
+  // }
+  // hacerAlgo(){
+  //   alert("haciendo algo")
+  // }
+  }
 }
